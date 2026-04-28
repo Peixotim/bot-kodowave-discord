dcd8dedbbdf3c105e6c61e90012b2172b000b8cb
Pedro Peixoto <pedropeixotovz@gmail.com>
2026-04-28 11:44:06 -0300

    feat: Add a function to load commands in the Discord client.

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,1 +1,13 @@
-export * from "./music/pause";
+import { Command } from "@/types";
+import { Client } from "discord.js";
+import play from "./music/play";
+
+const commands: Command[] = [play];
+
+export function loadCommands(client: Client) {
+  for (const command of commands) {
+    client.commands.set(command.data.name, command);
+  }
+
+  console.log(`${commands.length} commands carregados!`);
+}

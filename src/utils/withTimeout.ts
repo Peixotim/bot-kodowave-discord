988cffcea19a0fef691ca1cc68976a98791166ce
Pedro Peixoto <pedropeixotovz@gmail.com>
2026-04-29 17:28:29 -0300

    feat: adicionar função withTimeout para gerenciamento de promessas com tempo limite

---
+++ src/utils/withTimeout.ts
@@ -0,0 +1,7 @@
+export function withTimeout(ms: number): Promise<never> {
+  return new Promise((_, reject) =>
+    setTimeout(() => reject(new Error("TIMEOUT")), ms),
+  );
+}
+
+export default withTimeout;

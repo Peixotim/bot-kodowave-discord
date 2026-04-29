01332d71baddc20fa51b88683874eb356d19fc1b
Pedro Peixoto <pedropeixotovz@gmail.com>
2026-04-29 09:52:27 -0300

    feat: atualizar token de login e adicionar tratamento de eventos de processo

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
 import client from "./client";
 import { loadCommands } from "./commands";
 import { loadEvents } from "./events";
-import { token } from "./utils/token";
+import { DISCORD_TOKEN } from "./utils/token";
 
 loadEvents(client);
 loadCommands(client);
-client.login(token);
+client.login(DISCORD_TOKEN);
+
+process.on("SIGINT", async () => {
+  console.log("Desligando bot...");
+  client.voice.adapters.forEach((adapter) => adapter.destroy());
+  client.destroy();
+  process.exit(0);
+});
+
+process.on("SIGTERM", async () => {
+  console.log("Desligando bot...");
+  client.voice.adapters.forEach((adapter) => adapter.destroy());
+  client.destroy();
+  process.exit(0);
+});
+
+process.on("unhandledRejection", (error: Error) => {
+  console.error("Erro não tratado:", error.message);
+});
+
+process.on("uncaughtException", (error: Error) => {
+  console.error("Exceção não capturada:", error);
+});

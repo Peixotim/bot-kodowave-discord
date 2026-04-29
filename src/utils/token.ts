d7803b5d396b02d5527ecc61a1a277268790ded0
Pedro Peixoto <pedropeixotovz@gmail.com>
2026-04-29 09:53:18 -0300

    feat: refatorar exportação de tokens do Discord para incluir CLIENT_ID e GUILD_ID

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,2 +1,6 @@
 import "dotenv/config";
-export const token = process.env.DISCORD_TOKEN;
+
+//Fazer uma verificacao mais forte depois usando zod
+export const DISCORD_TOKEN = process.env.DISCORD_TOKEN as string;
+export const CLIENT_ID = process.env.CLIENT_ID as string;
+export const GUILD_ID = process.env.GUILD_ID as string;

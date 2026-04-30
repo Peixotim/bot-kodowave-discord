ffcc009c712953faabef6e6bb4f92d7fa2bd7eef
Peixotim <pedropeixotovz@gmail.com>
2026-04-30 13:57:53 -0300

    feat: adicionar validação de credenciais do SoundCloud com zod

--- src/utils/credentialsSoundCloud.ts
+++ src/utils/credentialsSoundCloud.ts
@@ -1,2 +1,13 @@
-export const SOUNDCLOUD_CLIENT_ID = process.env.SOUNDCLOUD_CLIENT_ID;
-export const SOUNDCLOUD_OAUTH_TOKEN = process.env.SOUNDCLOUD_OAUTH_TOKEN;
+import { z } from "zod";
+import "dotenv/config";
+
+const soundCloudSchema = z.object({
+  SOUNDCLOUD_CLIENT_ID: z
+    .string()
+    .min(1, "CONFIGURE O CLIENT_ID DO SOUNDCLOUD"),
+  SOUNDCLOUD_OAUTH_TOKEN: z
+    .string()
+    .min(1, "CONFIGURE O OAUTH_TOKEN DO SOUNDCLOUD"),
+});
+
+export const soundcloudEnv = soundCloudSchema.parse(process.env);

e670de04d177e61f3921cf293e36cf830787fbe4
Peixotim <pedropeixotovz@gmail.com>
2026-04-30 13:47:54 -0300

    fix: remover GUILD_ID da importação em deploy.ts

--- src/scripts/deploy.ts
+++ src/scripts/deploy.ts
@@ -1,13 +1,14 @@
 import skip from "../commands/music/skip";
 import play from "../commands/music/play";
 import { Command } from "../types";
-import { CLIENT_ID, GUILD_ID, DISCORD_TOKEN } from "../utils/token";
+import { CLIENT_ID, DISCORD_TOKEN } from "../utils/token";
 import { REST, Routes } from "discord.js";
 import pause from "../commands/music/pause";
 import queue from "../commands/music/queue";
 import stop from "../commands/music/stop";
 import guide from "../commands/music/guide";
 import loop from "../commands/music/loop";
+import "dotenv/config";
 
 const commands: Command[] = [play, skip, pause, queue, stop, guide, loop];
 const rest: REST = new REST({ version: "10" }).setToken(DISCORD_TOKEN);
